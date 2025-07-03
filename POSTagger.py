9eb4cf29693ab1eb25849a5a9c9cfb75fe7ee8e2
Darren Dube <darrendube@Darrens-MacBook-Air.local>
2025-07-03 23:04:30 +0200

    Finish core implementation and perform some light testing

--- POSTagger.py
+++ POSTagger.py
@@ -17,8 +17,9 @@ np.seterr(divide="ignore") # TODO: check if this is even necessary
 #
 
 class DiscreteRV:
-    def __init__(self, tokens:ndarray, probabilities:ndarray):
+    def __init__(self, tokens:np.ndarray, probabilities:np.ndarray):
         assert tokens.shape[0] == probabilities.shape[0], "Mismatch of lengths"
+        print(probabilities)
         assert abs(sum(probabilities) - 1.0) < 1e-7, "Probabilities \
          do not add up to 1"
         self.tokens = tokens
@@ -33,12 +34,22 @@ class DiscreteRV:
 
 
 class POSTagger:
-    def __init__(self, n_states):
-        self.n_states = n_states
-        # TODO: just initialise some local params e.g. max number of iterations, 
-        # error teolerance for viterbi decoding
+    '''
+    Class for classifying words in a text into their corresponding parts of speech.
+    This is based on the Hidden Markov Model, where the hidden states are the
+    parts of speech, and the observed variables are the words. 
+
+    Usage:
+    1. Initialise the class
+    2. Either use `load()` to load a transition matrix and list of emission
+    distributions, or train the model by passing training data to `fit()`
+    3. Use `get_tags()` to tag a given text with its parts of speech (uses Viterbi
+    decoding)
+    4. Use `sample` to generate sample texts/sentences. NOTE: the sentences generated 
+    are likely to be nonsensical as this clas doesn't model syntax, semantics, or context
+    '''
     
-    def fit(self, signals, tags):
+    def fit(self, signals:list, tags:list):
         '''
         Train the POS Tagger using the training data provided in `signal` and 
         `tags`.
@@ -54,9 +65,9 @@ class POSTagger:
         '''
         # TODO: count tag transitions and estimate state emission probabilities
         # from training data
-        self._unique_tags = np.unique(tags)
-        self.A = POSTagger._estimate_trans_matrix(tags)
-        self.state_dists = POSTagger._estimate_state_dists(signals, tags)
+        self._unique_tags = np.unique(np.concatenate(tags))
+        self.A = self._estimate_trans_matrix(tags)
+        self.state_dists = self._estimate_state_dists(signals, tags)
         
 
     def load_hmm(self, transition_matrix, state_dists):
@@ -74,7 +85,7 @@ class POSTagger:
 
         # initialisation
         for j in range(N):
-            Delt[j,0] = np.log(self.A[-1,j]) + self.state_dists[j].prob(signal[0])
+            Delt[j,0] = np.log(self.A[-1,j]) + self.state_dists[j].pmf(signal[0])
             Backp[j,0] = -1
 
         # recursion
@@ -83,7 +94,7 @@ class POSTagger:
                 vals = []
                 for i in range(N):
                     vals += [Delt[i,t-1] + np.log(self.A[i,j])]
-                Delt[j, t] = self.state_dists[j].prob(signal[t]) + np.max(vals)
+                Delt[j, t] = self.state_dists[j].pmf(signal[t]) + np.max(vals)
                 Backp[j, t] = int(np.argmax(vals))
 
         # termination
@@ -103,41 +114,58 @@ class POSTagger:
             curr = seq[-1]
             t -= 1
 
-        return self._int_to_tag(np.array(seq)[::-1]])
+        return self._int_to_tag(np.array(seq)[::-1])
 
-    @staticmethod
-    def _estimate_trans_matrix(tags):
-        N = len(self.state_dists)
+    def _estimate_trans_matrix(self, tags):
+        N = self._unique_tags.shape[0]
         k = 1 # k value for smoothing
-        state_seqs = self._tag_to_int(tags)
-
+        state_seqs = [self._tag_to_int(tag_seq) for tag_seq in tags]
         transition_counts = np.zeros((N+1, N+1))
+        # pad each state sequence with initial and terminal state
+        for i in range(len(state_seqs)):
+            state_seqs[i] = np.insert(state_seqs[i], 0, -1)
+            state_seqs[i] = np.append(state_seqs[i], N)
 
-        # pad each state sequence with 
-        for i in range(tags.shape[1]):
-            state_seqs[:,i] = np.insert(state_seqs[:,i], 0, -1)
-            state_seqs[:,i] = np.append(state_seqs, N)
-
-        for seq in state_seqs.T:
+        for seq in state_seqs:
             for i in range(seq.shape[0] - 1):
                 transition_counts[seq[i], seq[i+1]] += 1
         transition_counts += k # add smoothing k
 
         return transition_counts / transition_counts.sum(axis=1, keepdims=True)
 
+    def _estimate_state_dists(self, signals, tags):
+        signals_flat = np.concatenate(signals)
+        tags_flat = np.concatenate(tags)
+        grouped_vals = {}
+        state_dists = []
+        print(self._unique_tags)
+        for tag in self._unique_tags:
+            grouped_vals[tag] = signals_flat[np.where(tags_flat == tag)[0]]
+        for tag in self._unique_tags:
+            unique_tokens, counts = np.unique(grouped_vals[tag], return_counts=True)
+            probs = counts / counts.sum()
+            state_dists += [DiscreteRV(unique_tokens, probs)]
+        return state_dists
 
-
-    @staticmethod
-    def _estimate_state_dists(signals, tags):
-        # TODO: collect an array of tokens for each tag, then calculate 
-        # probabilities of words based on relative proportions, plus maybe a 
-        # small smoothing value k???
-        pass
-
-    def _int_to_tag(val):
+    def _int_to_tag(self, val):
         return self._unique_tags[val]
 
-    def _tag_to_int(tag):
-        return np.where(self._unique_tags == tag)[0][0]
+    def _tag_to_int(self, tag):
+        to_index = np.vectorize(lambda x: np.where(self._unique_tags == x)[0][0])
+        return to_index(tag)
 
+    def sample(self):
+        samples = []
+        states = []
+        curr_state = -1
+        while True:
+            state_trans_probs = self.A[curr_state]
+            # sample next state based on current transition probabilities
+            curr_state = np.random.choice(len(self._unique_tags) + 1, p=state_trans_probs)
+            if curr_state == len(self.state_dists):
+                states = np.array(states)
+                return np.array(samples), self._int_to_tag(states)
+            states += [curr_state]
+            samples += [self.state_dists[curr_state].sample()]
+        return samples, states
 
