067e36340a78218c80bfc6e19cd1f121f8749c86
Darren Dube <darrendube@Darrens-MacBook-Air.local>
2025-07-27 19:50:24 +0200

    Fix issue of not taking the log of the pmf value during Viterbi decoding

--- POSTagger.py
+++ POSTagger.py
@@ -1,25 +1,9 @@
 import numpy as np
-np.seterr(divide="ignore") # TODO: check if this is even necessary
-
-
-# XXX: NOTE that this case (parts-of-speech tagging) is a supervised problem, 
-# unlike the signature assignmetn in CS315 which was unsurpervied. in POS tagging,
-# we are given the tags, so we can speed up training by simply counting transitions and
-# estimating emission probabilities from the data. We can then load this information
-# through some function like load(). So the main point of this class is to find the most
-# likely state sequence for an unknown, test sequence, using viterbi
-#
-# XXX: in the load phase, remember to smooth after counting - in real data some 
-# state transitions or word emission smay never appear in training, resulting in 
-# a zero count, which makes porbabilities zero and thus certain paths impossible
-# (even though they may be possible in real life). Avoid this by adding a small value 
-# k to counts before normlisation 
-#
+np.seterr(divide="ignore") 
 
 class DiscreteRV:
     def __init__(self, tokens:np.ndarray, probabilities:np.ndarray):
         assert tokens.shape[0] == probabilities.shape[0], "Mismatch of lengths"
-        print(probabilities)
         assert abs(sum(probabilities) - 1.0) < 1e-7, "Probabilities \
          do not add up to 1"
         self.tokens = tokens
@@ -35,18 +19,16 @@ class DiscreteRV:
 
 class POSTagger:
     '''
-    Class for classifying words in a text into their corresponding parts of speech.
-    This is based on the Hidden Markov Model, where the hidden states are the
-    parts of speech, and the observed variables are the words. 
+    Parts of Speech Tagger based on the Hidden Markov Model.
 
     Usage:
-    1. Initialise the class
+    1. Initialise the class.
     2. Either use `load()` to load a transition matrix and list of emission
-    distributions, or train the model by passing training data to `fit()`
+    distributions, or train the model by passing training data to `fit()`.
     3. Use `get_tags()` to tag a given text with its parts of speech (uses Viterbi
-    decoding)
+    decoding).
     4. Use `sample` to generate sample texts/sentences. NOTE: the sentences generated 
-    are likely to be nonsensical as this clas doesn't model syntax, semantics, or context
+    are likely to be nonsensical as this class doesn't model syntax, semantics, or context.
     '''
     
     def fit(self, signals:list, tags:list):
@@ -63,29 +45,54 @@ class POSTagger:
         tags : str ndarray
             An array of tags corresponding to each token in `signal`
         '''
-        # TODO: count tag transitions and estimate state emission probabilities
-        # from training data
+        
+        lowercase_signals = [np.char.lower(arr) for arr in signals]
+
         self._unique_tags = np.unique(np.concatenate(tags))
         self.A = self._estimate_trans_matrix(tags)
-        self.state_dists = self._estimate_state_dists(signals, tags)
+        self.state_dists = self._estimate_state_dists(lowercase_signals, tags)
         
 
     def load_hmm(self, transition_matrix, state_dists):
+        '''
+        Initialise the POS Tagger using an already-generated transition matrix and state
+        dsitributions.
+
+        Parameters
+        ----------
+        transition_matrix : ndarray
+            A transition matrix. The last row must correspond to an "INIT" state, and the 
+            last column must correspond to a "TERMINAL" state.
+        state_dists : list
+            A list of `DiscreetRV` objects corresponding to the tags.
+        '''
         self.A = transition_matrix
         self.state_dists = state_dists
 
     def get_tags(self, signal:np.ndarray):
-        # TODO: store tags in an array then simply work with ints
-        np.seterr(invalid="ignore") # XXX: check if this is even necessary
+        '''
+        Assign parts-of-speech tags to a given sentence by performing Viterbi decoding.
+
+        Parameters
+        ----------
+        signal : ndarray
+            A 1D numpy array of String tokens - a token can be a word or punctuation mark.
+
+        Returns
+        -------
+        ndarray
+            Assigned parts-of-speech tags
+        '''
+        signal = np.char.lower(signal)
+        np.seterr(invalid="ignore") 
         N = len(self.state_dists)
         T = signal.shape[0]
-        
         Delt = np.zeros((N, T))
         Backp = np.zeros((N, T), dtype=int)
 
         # initialisation
         for j in range(N):
-            Delt[j,0] = np.log(self.A[-1,j]) + self.state_dists[j].pmf(signal[0])
+            Delt[j,0] = np.log(self.A[-1,j]) + np.log(self.state_dists[j].pmf(signal[0]))
             Backp[j,0] = -1
 
         # recursion
@@ -94,7 +101,7 @@ class POSTagger:
                 vals = []
                 for i in range(N):
                     vals += [Delt[i,t-1] + np.log(self.A[i,j])]
-                Delt[j, t] = self.state_dists[j].pmf(signal[t]) + np.max(vals)
+                Delt[j, t] = np.log(self.state_dists[j].pmf(signal[t])) + np.max(vals)
                 Backp[j, t] = int(np.argmax(vals))
 
         # termination
@@ -117,6 +124,9 @@ class POSTagger:
         return self._int_to_tag(np.array(seq)[::-1])
 
     def _estimate_trans_matrix(self, tags):
+        '''
+
+        '''
         N = self._unique_tags.shape[0]
         k = 1 # k value for smoothing
         state_seqs = [self._tag_to_int(tag_seq) for tag_seq in tags]
@@ -138,7 +148,6 @@ class POSTagger:
         tags_flat = np.concatenate(tags)
         grouped_vals = {}
         state_dists = []
-        print(self._unique_tags)
         for tag in self._unique_tags:
             grouped_vals[tag] = signals_flat[np.where(tags_flat == tag)[0]]
         for tag in self._unique_tags:
